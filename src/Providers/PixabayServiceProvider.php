d980f482c6b4403796e4d9c62d5b6a1319cf4a01
mikeyperes <mikeyperes@users.noreply.github.com>
2026-04-27 03:29:39 +0000

    feat(pixabay): register package metadata

--- src/Providers/PixabayServiceProvider.php
+++ src/Providers/PixabayServiceProvider.php
@@ -42,6 +42,13 @@ class PixabayServiceProvider extends ServiceProvider
             // HWS-SIDEBAR-MENU-3L-END
 
             $registry->registerSidebarLink('pixabay.index', 'Pixabay', 'M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z', 'Sandbox', 'pixabay', 84);
+            $registry->registerPackage('pixabay', 'hexawebsystems/laravel-hexa-package-pixabay', [
+                'title' => 'Pixabay',
+                'description' => 'Stock image search and import tooling powered by the Pixabay API.',
+                'icon' => 'M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z',
+                'color' => 'amber',
+                'settingsRoute' => 'pixabay.index',
+            ]);
         }
     }
 }
